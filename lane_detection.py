e95e7e648bdbc3cf72bfb5cbd60e2ed9298c1163
tangj-mvla <joshuatang2007@gmail.com>
2024-07-26 11:03:05 -0400

    Stuff added

---
+++ lane_detection.py
@@ -0,0 +1,14 @@
+def detect_lines(img, threshold1, threshold2, aperatureSize, minLineLength,maxLineGap):
+    pass
+
+def draw_lines(img,lines, color):
+    pass
+
+def get_slopes_intercepts(lines):
+    pass
+
+def detect_lanes(lines):
+    pass
+
+def draw_lanes(img, lanes):
+    pass
